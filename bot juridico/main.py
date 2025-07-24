35dac7f397b6a3593db4decb9485fa93120a50e6
mizaflix <mizaflix@gmail.com>
2025-07-24 10:52:23 -0300

    Update main.py

--- bot juridico/main.py
+++ bot juridico/main.py
@@ -122,4 +122,5 @@ def bot():
     return str(resp)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
